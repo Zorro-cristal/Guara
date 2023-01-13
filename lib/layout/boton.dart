0ce225ea7227b0a0c092fa1c797f52830f8ae35e
Alejandro Alvarez <zorro-cristal@hotmail.com>
2023-01-13 19:10:38 -0300

    Boton agregado

---
+++ lib/layout/boton.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class Boton extends StatelessWidget {
+  final Function ruta;
+  final String titulo;
+
+  const Boton({super.key, required this.ruta, required this.titulo});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 350,
+      decoration: BoxDecoration(
+          border: Border.all(
+            //color: Colors.blue[900],
+            color: Colors.blue,
+            width: 8,
+          ),
+          color: Colors.grey,
+          borderRadius: BorderRadius.circular(35)),
+      child: TextButton(
+        onPressed: ruta(),
+        /* onPressed: () {
+          Navigator.push(context, 
+            MaterialPageRoute(builder: (context) => EspanholGuaraniHome())
+          );
+        }, */
+        //padding: const EdgeInsets.all(8.0),
+        child: Text(
+          titulo,
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 28.5,
+            fontStyle: FontStyle.italic,
+            color: Colors.black
+          ),
+        ),
+        //)
+      ),
+    );
+  }
+}
