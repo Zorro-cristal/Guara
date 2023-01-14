f179235e8c4feb473b991678bdfab2804fe831a3
Alejandro Alvarez <zorro-cristal@hotmail.com>
2023-01-14 10:08:57 -0300

    Pantalla principal agregada

---
+++ lib/screen/principal.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:guara2/layout/boton.dart';
+
+class Principal extends StatelessWidget {
+  const Principal({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage("assets/images/principal.jpg"),
+                    fit: BoxFit.cover)),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Boton(
+                        presionado: () => null, titulo: "Castellano a guarani")
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Boton(
+                        presionado: () => null,
+                        titulo: "Guaranigui castellanope")
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Boton(presionado: () => null, titulo: "Numeros/Papaha")
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [Boton(presionado: () => null, titulo: "Frases")],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Boton(presionado: () => null, titulo: "Sugerencias/Ñembohape")
+                  ],
+                ),
+              ],
+            )),
+      ),
+    );
+  }
+}
