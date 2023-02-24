06129ab7abc89cf65945710b8fe2c31da1ad3572
Alejandro Alvarez <zorro-cristal@hotmail.com>
2023-02-24 15:44:28 -0300

    Correcciones en buscador actualizado

--- lib/components/busqueda.dart
+++ lib/components/busqueda.dart
@@ -23,6 +23,7 @@ class _BusquedaState extends State<Busqueda> {
 
   String deficinion = "";
   String imagen = "";
+  bool sustant = false;
 
   // Retorna el primer resultado
   Future<void> busquedaApi() async {
@@ -31,9 +32,10 @@ class _BusquedaState extends State<Busqueda> {
     var cliente = clientViaApiKey(api_key_token);
     var api = new CustomSearchApi(cliente);
     var buscador = api.cse
-        .list(cx: motor_busqueda, q: widget.palabra, hl: "es", hq: "definicion")
+        .list(cx: motor_busqueda, q: widget.palabra, hl: "es", hq: "definicion", siteSearch: "rae.es", siteSearchFilter: "i")
         .then((Search busq) {
       if (busq.items != null) {
+        // Obtenemos el primer resultado
         resultado = Paquete_google_search(
             titulo: busq.items![0].title,
             url: busq.items![0].link,
@@ -41,17 +43,33 @@ class _BusquedaState extends State<Busqueda> {
         setState(() {
           deficinion = resultado.definicion();
         });
+        for (var result in busq.items!) {
+          print(result.snippet);
+          if (!sustant &&
+              (result.snippet!.contains("sustantivo") ||
+                  result.snippet!.contains("sust"))) {
+            print(result.snippet);
+            setState(() {
+              sustant = true;
+            });
+          }
+        }
+        print(sustant);
       }
     });
     buscador = api.cse
-        .list(cx: motor_busqueda, q: widget.palabra, hl: "es", hq: "fotos", searchType: "image")
+        .list(
+            cx: motor_busqueda,
+            q: widget.palabra,
+            hl: "es",
+            hq: "fotos",
+            searchType: "image", imgSize: "large")
         .then((Search busq) {
       if (busq.items != null) {
         resultado = Paquete_google_search(
             titulo: busq.items![0].title,
             url: busq.items![0].link,
             snippet: busq.items![0].snippet);
-        print(resultado.url);
         setState(() {
           imagen = resultado.url;
         });
